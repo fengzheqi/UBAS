1ab2f6a92caaef10dffa93d83ea540023e93e008
csdc04 <csdc04>
2016-10-17 17:30:36 +0800

    完成用户模块的接口

--- server/controllers/user/user.server.controllers.js
+++ server/controllers/user/user.server.controllers.js
@@ -8,7 +8,64 @@
 
 var mongoose = require('mongoose');
 var passport = require('passport');
+var _ = require('lodash');
 var User = mongoose.model('User');
+const async = require('async');
+
+/**
+ * 查找用户
+ * @param req
+ * @param res
+ * @param next
+ * @param id
+ */
+exports.userByID = function (req, res, next, id) {
+    User.findOne({
+        _id: id
+    }).exec(function (err, user) {
+        if (err) return next(err);
+        if (!user) return next(new Error('载入用户信息失败'));
+        req.profile = user;
+        next();
+    });
+};
+
+/**
+ * 验证用户是否登陆
+ * @param req
+ * @param res
+ * @param next
+ * @returns {*}
+ */
+exports.requireLogin = function (req, res, next) {
+    if (!req.isAuthenticated() || !req.user.isActive) {
+        return res.status(401).send({
+            '提示信息': '用户未登陆'
+        });
+    }
+    next();
+};
+
+/**
+ * 用户是否通过验证
+ * @param roles
+ * @returns {Function}
+ */
+exports.hasAuthorization = function (roles) {
+    var _this = this;
+
+    return function (req, res, next) {
+        _this.requireLogin(req, res, function () {
+            if(_.intersection(req.user.roles, roles).length) {
+                return next();
+            } else {
+                return res.status(403).send({
+                    '提示信息': '用户没有通过验证'
+                });
+            }
+        })
+    }
+};
 
 /**
  * 注册
@@ -55,7 +112,7 @@ exports.signin = function (req, res, next) {
             })
         }
     })(req, res, next);
-}
+};
 
 /**
  * 登出
@@ -65,4 +122,69 @@ exports.signin = function (req, res, next) {
 exports.signout = function (req, res) {
     req.logout();
     res.redirect('/');
+};
+
+/**
+ * 忘记密码
+ * @param req
+ * @param res
+ * @param next
+ */
+exports.forgot = function (req, res, next) {
+
+};
+
+/**
+ * 验证重置密码Token
+ * @param req
+ * @param res
+ */
+exports.validateResetToken = function (req, res) {
+
+};
+
+/**
+ * 重置密码
+ * @param req
+ * @param res
+ * @param next
+ */
+exports.reset = function (req, res, next) {
+
+};
+
+/**
+ * 修改密码
+ * @param req
+ * @param res
+ */
+exports.changePassword = function (req, res) {
+
+}
+
+/**
+ * 用户添加和更新
+ * @param req
+ * @param res
+ */
+exports.update = function (req, res) {
+
+}
+
+/**
+ * 查询用户个人信息
+ * @param req
+ * @param res
+ */
+exports.me  =function (req, res) {
+
+};
+
+/**
+ * 查询所有用户信息
+ * @param req
+ * @param res
+ */
+exports.list = function (req, res) {
+
 }
--- server/routes/user.js
+++ server/routes/user.js
@@ -12,6 +12,20 @@ const fail = {
 };
 
 /* Expose */
-module.exports = function () {
+module.exports = function (app) {
+    app.router('/user/me').get(users.requireLogin, users.me);
+    app.router('/user')
+        .get(users.requireLogin, users.list)
+        .put(user.requireLogin, users.update);
 
+    //用户认证
+    app.router('/auth/signup').post(users.signup());
+    app.router('/auth/signin').post(users.signin());
+    app.router('/auth/signout').get(users.signout());
+
+    //用户密码相关API
+    app.router('/user/changePassword').post(users.requireLogin, users.changePassword);
+    app.router('/user/forgot').post(users.forgot);
+    app.router('/user/reset/:token').get(users.validateResetToken);
+    app.router('/user/reset/?token').post(user.reset);
 }
