1daa9175a56f5f7bc8fca5956a675c5684b1c5b9
tommyzqfeng <fengzheqiyx@163.com>
2016-12-06 21:49:14 +0800

    修改系统上报域名

--- static/template.js
+++ static/template.js
@@ -9,7 +9,7 @@
   var s = document.createElement('script');
   s.id = 'feDataReport';
   s.type= 'text/javascript';
-  s.src= 'https://ubas.herokuapp.com/static/reportData.js?appId=811756202';
+  s.src= 'http://ubas.fengzheqi.com/static/reportData.js?appId=811756202';
   document.body.appendChild(s);
   s = null;
 })();
